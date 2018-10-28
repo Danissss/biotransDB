5b96652b2495003a4a278fc9bda235a6ba6466f5
Danissss <danis.cao@hotmail.com>
2018-10-28 16:38:56 -0600

    add the function that can check other dataset file (in csv)

--- Create_dataset_from_DB/COMBINE_DATASET/combineTwoDataset.py
+++ Create_dataset_from_DB/COMBINE_DATASET/combineTwoDataset.py
@@ -15,7 +15,80 @@ from rdkit import Chem
 from rdkit.Chem.Fingerprints import FingerprintMols
 # this use Topological Fingerprints to determine the similarity
 
-def combine_via_chemsimilarity(file_name):
+
+# this function is trying to combine the new dataset
+# to ensure that there is no redundent molecules;
+def combined_new_dataset(file_name):
+	checked_file = open("final_with_other_dataset.csv","w",newline='')
+	csv_writer_checked = csv.writer(checked_file,quoting=csv.QUOTE_ALL)
+
+	final_csv = open("final_.csv", newline='')
+	final_csvreader = csv.reader(final_csv, delimiter=',')
+	Original = []
+	# append instance from final_.csv
+	for row in final_csvreader:
+		Original.append(row[1])
+
+
+	# check for other dataset:
+	for file in file_name:
+		ab_csv = open(file, newline='')
+		ab_csvreader = csv.reader(ab_csv, delimiter=',')
+		tmp_list   = []
+		for row in ab_csvreader:
+			tmp_list.append(row[1])
+		for cl in Original:
+			for db in tmp_list:
+				mol_object_c = Chem.MolFromSmiles(cl)
+				mol_object_d = Chem.MolFromSmiles(db)
+
+				fps_c = FingerprintMols.FingerprintMol(mol_object_c)
+				fps_d = FingerprintMols.FingerprintMol(mol_object_d)
+				similiarty  = DataStructs.FingerprintSimilarity(fps_c,fps_d)
+				if similiarty == 1:
+					tmp_list.remove(db)
+		print("remaining compound from drugbank is: "+str(len(tmp_list)))
+		# append new compound into Original list
+		for remains in tmp_list:
+			Original.append(remains)
+
+
+
+
+		ab_csv.close()
+
+
+	# write to new file 
+	# remove the original similes from Original list 
+	for i in final_csvreader:
+		csv_writer_checked.writerow(i)
+		i_similes = i[1]
+
+		Original.remove(i_similes)
+
+	for file in file_name:
+		ab_csv = open(file, newline='')
+		ab_csvreader = csv.reader(ab_csv, delimiter=',')
+		for row in ab_csvreader:
+			row_similes = row[1]
+			if row_similes in Original:
+				csv_writer_checked.writerow(row)
+				Original.remove(row_similes)
+			else:
+				continue
+		ab_csv.close()
+
+	csv_write_file.close()
+	checked_file.close()
+	final_csv.close()
+
+
+
+
+
+
+# this function combine drugbank and chembl dataset
+def combine_via_chemsimilarity():
 	csv_write_file = open("Pairs.csv","w",newline='')
 	csv_writer = csv.writer(csv_write_file,quoting=csv.QUOTE_ALL)
 	checked_file = open("final_.csv","w",newline='')
@@ -41,29 +114,18 @@ def combine_via_chemsimilarity(file_name):
 	# later need automatically store into file 
 	for cl in CHEMBL:
 		for db in DRUGBANK:
-			# print("CHEMBL________________")
-			# print(cl)
-			# print("DRUGBANK________________")
-			# print(db)
 			mol_object_c = Chem.MolFromSmiles(cl)
 			mol_object_d = Chem.MolFromSmiles(db)
 
 			fps_c = FingerprintMols.FingerprintMol(mol_object_c)
 			fps_d = FingerprintMols.FingerprintMol(mol_object_d)
 			similiarty  = DataStructs.FingerprintSimilarity(fps_c,fps_d)
-			# print(similiarty)
-			# print("end======================")
-
 			if similiarty == 1:
 				db_list = list(db)
 				csv_writer.writerow(db_list)
 				DRUGBANK.remove(db)
 
 
-			# 	# print(cl,db)
-			# else:
-			# 	continue
-			# sys.exit(0)
 	print("remaining compound from drugbank is: "+str(len(DRUGBANK)))
 	for i in ChEMBL_csvreader:
 		csv_writer_checked.writerow(i)
@@ -147,7 +209,8 @@ def main():
 	# print(file_list)  # print correct csv file name 
 	# sys.exit(0)
 	# combine(file_list)
-	combine_via_chemsimilarity(file_list)
+	combine_via_chemsimilarity()
+	combined_new_dataset(file_list)
 
 
 
